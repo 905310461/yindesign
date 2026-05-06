79ebca0350add4a88c5892c9485cd8af0ffcbb43
你的用户名 <904310461@qq.com>
2026-05-06 11:09:26 +0800

    添加了新的作品图片

---
+++ js/jsfor_rem.js
@@ -0,0 +1,10 @@
+
+ /* 长宽占位 rem算法, 根据root的rem来计算各元素相对rem, 默认html 320/20 = 16px */
+ function placeholderPic(){
+  var w = document.documentElement.offsetWidth;
+  document.documentElement.style.fontSize=w/20+'px';
+ }
+ placeholderPic();
+ window.onresize=function(){
+  placeholderPic();
+ }
